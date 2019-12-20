535960d7d30993d8ac61b0f50f588c2933e4c35b
Olatunji <sirhollyfat@gmail.com>
2019-12-20 11:07:51 +0100

    Implement hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("My First App"),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-        "Hello Ninjas!",
-        style: TextStyle(
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
-          color: Colors.grey[600],
-          fontFamily: 'IndieFlower'
-        ),
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      child: Text('click'),
-      backgroundColor: Colors.red[600],
-    ),
-  ),
+  home: Home()
 ));
 
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My First App"),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+          "Hello Ninjas!",
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower'
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
   }
 }

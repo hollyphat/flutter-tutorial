c4ac6d8afc583de0bf2bd159fd7521ec9d97ebae
Olatunji <sirhollyfat@gmail.com>
2019-12-20 12:19:44 +0100

    Add padding widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,8 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Container(
-        padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
-        margin: EdgeInsets.all(20.0),
-        color: Colors.grey[400],
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
         child: Text("Hello"),
       ),
       floatingActionButton: FloatingActionButton(

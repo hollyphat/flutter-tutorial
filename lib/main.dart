e031e44da809bee2f4d9815de4549ac0522e0faa
Olatunji <sirhollyfat@gmail.com>
2019-12-20 12:16:17 +0100

    Add container padding

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Container(
-
+        padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
+        color: Colors.grey[400],
+        child: Text("Hello"),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},

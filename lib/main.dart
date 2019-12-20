b9d4889f188ac18e26e2f52284be9b2146937aed
Olatunji <sirhollyfat@gmail.com>
2019-12-20 11:03:16 +0100

    Create home(stateless) widget

--- lib/main.dart
+++ lib/main.dart
@@ -26,3 +26,11 @@ void main() => runApp(MaterialApp(
     ),
   ),
 ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
